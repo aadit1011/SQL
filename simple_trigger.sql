ee6ba37a93fbf6b01aa9348bcae90eb4b785d7d4
Aadit Sharma <106479910+aadit1011@users.noreply.github.com>
2024-09-23 20:25:05 +0545

    simple_trigger.sql

---
+++ simple_trigger.sql
@@ -0,0 +1,20 @@
+DELIMITER $$  
+CREATE TRIGGER first_trigger BEFORE INSERT ON employee
+    FOR EACH ROW
+BEGIN
+    INSERT INTO first_trigger VALUES ('Added employee'); -- Semicolon here does not end the trigger
+END$$                -- This $$ ends the trigger creation
+DELIMITER ;           -- Reset delimiter back to the default semicolon
+
+
+
+-- OR
+DELIMITER @  
+
+CREATE TRIGGER second_trigger BEFORE INSERT ON employee
+    FOR EACH ROW
+BEGIN
+    INSERT INTO trigger_log VALUES ('Employee added');
+END@
+
+DELIMITER ;
